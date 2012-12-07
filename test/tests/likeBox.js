2913c19d166d2230b453512536ece87d418c6634
K.Adam White <kadamwhite@gmail.com>
2012-12-07 16:05:07 -0500

    Initial tests for likebox

--- test/tests/likeBox.js
+++ test/tests/likeBox.js
@@ -9,7 +9,8 @@ define([
     var lb;
 
     setup(function() {
-      lb = LikeBox();
+      ul = $('<ul><li class="no-results"></li></ul>');
+      lb = LikeBox( ul );
     });
 
     teardown(function() {
@@ -18,6 +19,25 @@ define([
     test( 'constructor', function() {
       assert( lb );
       assert( lb instanceof LikeBox );
+    });
+
+    test( 'Should start with no liked items', function() {
+      // More to assure we don't do anything stupid later than to prove it works now
+      assert.equal( lb.el.find('.no-results').length, 1 );
+      assert.equal( lb.el.find('li').length, 1 );
+    });
+
+    test( 'Should display liked items', function() {
+      var testName = 'KAdam White';
+      lb.add( testName );
+
+      // Make sure the "no results" text has been removed
+      assert.equal( ul.find('.no-results').length, 0, 'no-results text is cleared' );
+      // Doesn't just check that there's the right number, but also (implicitly) that
+      // the appropriate .results class is present on the rendered list items
+      assert.equal( ul.find('li').length, 1, 'liked item is added' );
+      // Check to make sure the name gets rendered
+      assert.equal( ul.find('li').first().html(), testName, 'name is rendered' );
     });
 
   });
