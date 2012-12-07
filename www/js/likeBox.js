344c0e53ecd36fbab9b3a0bb260f43a3e8d94db3
K.Adam White <kadamwhite@gmail.com>
2012-12-07 16:17:17 -0500

    Add impl for blocking likes of previously liked names

--- www/js/likeBox.js
+++ www/js/likeBox.js
@@ -7,12 +7,18 @@ define([ 'jquery' ], function( $ ) {
       return new LikeBox( el );
     }
 
+    this._names = {};
+
     this.el = $( el );
   };
 
   LikeBox.prototype.add = function( name ) {
+    if( this._names[ name ] ) { return; }
+    // Overhead of this is so minimal it is barely worth it to run
+    // this in some smarter way that would only call it once
     this.el.find( '.no-results' ).remove();
     this.el.append( '<li>' + name + '</li>' );
+    this._names[ name ] = name;
   };
 
   return LikeBox;
