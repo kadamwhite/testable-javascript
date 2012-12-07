17cc3eb24f67245d801005b6582030b081e15188
K.Adam White <kadamwhite@gmail.com>
2012-12-07 11:44:34 -0500

    Enumerate more tests we want to write, with failures

--- test/tests/searchForm.js
+++ test/tests/searchForm.js
@@ -18,9 +18,31 @@ define([ 'searchForm' ], function( SearchForm ) {
     });
 
     // If I submit with text in the field, I expect for an event to get triggered
+    test( 'Search event is triggered with query', function() {
+      assert.fail();
+    });
 
     // If I submit with no text in the field, I expect for an event NOT to get triggered
     //   (Or text where the trimmed query is empty, e.g. has no spaces)
+    test( 'No search event is triggered with empty query', function() {
+      assert.fail();
+    });
 
+    // Pending search issue is tricky: You don't want the form to have to know
+    // about the ongoing search, but you do want to find a way to prevent a
+    // search from going out before the previous one completes. Rebecca has used
+    // a technique where you lock the search form and then unlock it later to
+    // work around this problem.
+    suite( 'Locking', function() {
+      test( 'Form is locked', function() {
+        assert.fail();
+      });
+      test( 'Form can be unlocked', function() {
+        assert.fail();
+      });
+      test( 'Locking prevents search', function() {
+        assert.fail();
+      });
+    });
   });
 });
