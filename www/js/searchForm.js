5b3d02a2ee55c1886e44dae6af13d49998895336
K.Adam White <kadamwhite@gmail.com>
2012-12-08 15:27:53 -0500

    Implement Locking

--- www/js/searchForm.js
+++ www/js/searchForm.js
@@ -7,12 +7,13 @@ define([
   'rsvp'
 ], function( $, _, RSVP ) {
   var SearchForm = function( form ) {
-    if( !(this instanceof SearchForm ) ) {
+    if( !(this instanceof SearchForm) ) {
       return new SearchForm( form );
     }
 
     this._form = $( form );
     this._form.on( 'submit', _.bind( this._handleSubmit, this ) );
+    this._locked = false;
 
     return this;
   };
@@ -24,12 +25,19 @@ define([
 
     // We should store the form as something
     var query = $.trim( this._form.find('[name="q"]').val() );
-    if( !query ) {
+    if( !query || this._locked ) {
       return;
     }
     this.trigger( 'search', query );
-    // Because we've written this trigger method but haven't defined it yet,
-    // we're going to pull in RSVP
+    // .trigger is enabled by pulling in RSVP (see below)
+  };
+
+  SearchForm.prototype.lock = function() {
+    this._locked = true;
+  };
+
+  SearchForm.prototype.unlock = function() {
+    this._locked = false;
   };
 
   RSVP.EventTarget.mixin( SearchForm.prototype );
@@ -37,6 +45,7 @@ define([
   // You can write this yourself in a basic working version in 20-30
   // lines of code; if you want to .off stuff and handle memory better
   // it gets slightly more complicated
+  // https://github.com/tildeio/rsvp.js
 
   return SearchForm;
 });
